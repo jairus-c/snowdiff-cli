5aae408f6bd1cc37c96b23cc5c5322bafb5fc63e
jairus-m <jairusmartinez@gmail.com>
2024-05-08 17:20:58 -0700

    Created unittests / mocks

---
+++ tests/main_test.py
@@ -0,0 +1,56 @@
+import os
+import pytest
+from unittest.mock import patch, MagicMock, mock_open
+from argparse import Namespace
+from src.utils import expected_profile, snowflake_connector
+from src.__main__ import parse_arguments, get_user_input, load_profile_data, main
+
+try:
+    username = os.getenv("USER")
+    if not username:
+        username = os.getenv("USERNAME")
+    if not username:
+        raise ValueError("Environment variable for user is not set.")
+except ValueError as e:
+    print(e)
+
+@pytest.fixture
+def mock_args():
+    args = Namespace(table='test_table', filter='test_filter', custom_schema='test_schema')
+    return args
+
+@patch('argparse.ArgumentParser.parse_args')
+def test_parse_arguments(mock_parse_args):
+    mock_parse_args.return_value = Namespace(table='test_table', filter='test_filter', custom_schema='test_schema')
+    assert parse_arguments() == mock_parse_args.return_value
+
+@patch('builtins.input', side_effect=['test_table', 'test_schema', 'test_filter'])
+def test_get_user_input(mock_input):
+    assert get_user_input() == ('test_schema', 'test_table', 'test_filter')
+
+# @patch('builtins.open', new_callable=mock_open)
+# @patch('os.getenv', MagicMock(return_value='test_user'))
+# def test_load_profile_data(mock_open):
+#     mock_open.return_value.read.return_value = """
+#     default:
+#       outputs:
+#         dev:
+#           account: test_account
+#           password: test_password
+#           user: test_user
+#           warehouse: test_warehouse
+#           schema: test_schema
+#         prod:
+#           schema: test_schema_prod
+#     """
+#     assert load_profile_data('test_user') == ('test_user', 'test_account', 'test_warehouse', 'test_password', 'test_schema_prod', 'test_schema')
+
+@patch('os.getenv', MagicMock(return_value=username))
+@patch('src.utils.snowflake_connector.SnowflakeConnector')
+@patch('argparse.ArgumentParser.parse_args')
+def test_main(mock_parse_args, mock_snowflake_connector):
+    mock_parse_args.return_value = Namespace(table='test_table', filter='test_filter', custom_schema=None)
+    mock_snowflake_connector_instance = MagicMock()
+    mock_snowflake_connector.return_value = mock_snowflake_connector_instance
+    main()
+    mock_snowflake_connector_instance.query.assert_called()
